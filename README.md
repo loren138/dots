684cc5894294681475ca3462e235afb824a5548e
Joshua Cottrell <joshua.e.cottrell@gmail.com>
2016-05-09 13:21:53 -0400

    Added setup script

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Simple setup for Ubuntu servers
+* Clone this repo `git clone https://github.com/jcottrell/dots.git`
+* `cd dots`
+* `chmod +x setup.sh`
+* Run either `./setup.sh --server` or ./setup.sh --mac` depending on the system
 
 ## Set Editor to something useful
 sudo apt-get install vim
---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+myDir=`pwd`
+home=$HOME
+
+addLink()
+{
+    if [ -L $home/$1 ] || [ -f $home/$1 ] || [ -d $home/$1 ]; then
+        echo "$home/$1 already exists"
+    elif [ ! -f $myDir/$1 ] && [ ! -d $myDir/$1 ]; then
+        echo "$myDir/$1 is not a file or directory"
+    else
+        echo "Adding $1"
+        if [ -z "$2" ]; then
+            ln -s $myDir/$1 $home/$1
+        else
+            ln -s $myDir/$1 $home/$2
+        fi
+    fi
+}
+# main
+# # eventually needs full argument handling: only add certain things;
+# # remove .bashrc, etc.
+if [ "$1" = "--server" ] || [ "$1" = "--mac" ]; then
+    # if server then connect up .bashrc-server and .vimrc-server
+    if [ $1 = "--server" ]; then
+        addLink ".bashrc-server" ".bashrc"
+        addLink ".vimrc-server" ".vimrc"
+    else
+    # else connect up .bash_profile-maxosx and .vimrc-macvim
+        addLink ".bash_profile-maxosx" ".bash_profile"
+        addLink ".vimrc-macvim" ".vimrc"
+    fi
+    addLink ".bash_prompt_colors"
+    addLink ".ctags"
+    addLink ".dircolors"
+    # addLink ".editorconfig" # ?? for vim-jsbeautify
+    addLink ".git_template"
+    addLink ".gitconfig"
+    addLink ".vim"
+    addLink "bin"
+else
+    echo "Please supply either --server or --mac as the first parameter"
+    echo "$1"
+fi
